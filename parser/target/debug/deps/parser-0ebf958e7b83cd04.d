cc90df1922cff9ef3c9b671c155a28aca85b8d4d
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-05-28 17:57:35 -0700

    Fixed {}; colors

--- parser/target/debug/deps/parser-0ebf958e7b83cd04.d
+++ parser/target/debug/deps/parser-0ebf958e7b83cd04.d
@@ -1,9 +1,10 @@
-/Users/KayK/Programs/ecs140project/parser/target/debug/deps/parser-0ebf958e7b83cd04: src/main.rs src/cstream.rs src/token.rs src/scanner.rs src/parser.rs
+/Users/KayK/Programs/ecs140project/parser/target/debug/deps/parser-0ebf958e7b83cd04: src/main.rs src/cstream.rs src/token.rs src/scanner.rs src/parser.rs src/create_xhtml.rs
 
-/Users/KayK/Programs/ecs140project/parser/target/debug/deps/parser-0ebf958e7b83cd04.d: src/main.rs src/cstream.rs src/token.rs src/scanner.rs src/parser.rs
+/Users/KayK/Programs/ecs140project/parser/target/debug/deps/parser-0ebf958e7b83cd04.d: src/main.rs src/cstream.rs src/token.rs src/scanner.rs src/parser.rs src/create_xhtml.rs
 
 src/main.rs:
 src/cstream.rs:
 src/token.rs:
 src/scanner.rs:
 src/parser.rs:
+src/create_xhtml.rs:
