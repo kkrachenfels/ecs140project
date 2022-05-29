7afb4dd2dfc8a9d76ce0dce2be4b783ffa632416
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-05-28 19:42:21 -0700

    Print file to screen

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -15,16 +15,20 @@ use crate::parser::*;
 use crate::create_xhtml::create;
 
 fn main() {
+    //initialize all_tokens vector
     let mut all_tokens:Vec<Token>=vec![];
     let args: Vec<String> = env::args().collect();
+    
+    //print out file to screen
     let mut s = Scanner::new(&args[1]);
+    println!("{}", s.cstrm.file_str);
+
+    //read in and store tokens into all_tokens vector
     while s.not_eof() {
         all_tokens.push(s.get_next_token());
     }
-    //read in all tokens
-    while s.not_eof() {
-        all_tokens.push(s.get_next_token());
-    }
+    //end of getting tokens, all tokens should now be stored in all_tokens
+    
     //print out collected tokens
     let mut n=0;
     for token in all_tokens.iter()
@@ -32,11 +36,14 @@ fn main() {
         println!("Token {} = {}\nToken Type: {:?}\n",n,token.text,token.token_type);
         n+=1;
     }
+
     //parse the vector of tokens
     let mut x_parser = Parser::new(&all_tokens);
     let result = x_parser.program();
+    
+    //if syntactically correct, create the xhtml file
     match result {
-        Ok(_) => (create(all_tokens)),
-        Err(e) => println!("{}", e) 
+        Ok(_) => (create(all_tokens)), //create xhtml
+        Err(e) => println!("{}", e) //throw the custom err
     }
 }
